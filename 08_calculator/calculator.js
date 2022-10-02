f1a5a79d6727084cbbcd13f36466d269fd815f4a
Narendra Bharathi C V <narendra.bharathi@gmail.com>
2022-10-02 00:12:28 -0400

    Implement factorial op to the calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -57,8 +57,17 @@ const power = function(a, b) {
   return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+
+  let fact = 1;
+  for (let i = 1; i <= num; i++) {
+    fact = fact * i;
+  }
+
+  return fact;
 };
 
 // Do not edit below this line
