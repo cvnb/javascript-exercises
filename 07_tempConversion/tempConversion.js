bc8d1104d07deed13a8e6b26eac47c44fc3c6a02
Narendra Bharathi C V <narendra.bharathi@gmail.com>
2022-10-01 23:09:46 -0400

    Implement functions to convert temperatures from/to fahrenheit/celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const round = function (num) {
+  return Math.round(num + Number.EPSILON) * 10 / 10;
+}
 
+const ftoc = function (f) {
+  let c = (f - 32) * 5 / 9;
+  return round(c);
 };
 
-const ctof = function() {
-
+const ctof = function (c) {
+  let f = (c * 9 / 5) + 32;
+  return round(f);
 };
 
 // Do not edit below this line
