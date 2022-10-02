f6bea0ed9a32db273091adec6871b8d4a9552bcf
Narendra Bharathi C V <narendra.bharathi@gmail.com>
2022-10-02 00:21:29 -0400

    Implement fn to test if argument is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] != str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
 
 };
 
