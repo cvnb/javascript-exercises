f0c189aa2e252baef2250e3b61166a4d47d45393
Narendra Bharathi C V <narendra.bharathi@gmail.com>
2022-10-01 22:01:21 -0400

    Add logic to error for negative count argument

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,8 @@
 const repeatString = function (string, count) {
+    if (count < 0) {
+        return 'ERROR';
+    };
+    
     let result = '';
     for (let i = 0; i < count; i++) {
         result = result + string;
